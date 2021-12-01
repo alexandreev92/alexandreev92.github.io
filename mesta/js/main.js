796c61b66610f77c9bb67ebefba4d126d31a17a7
alexandreev92 <93050970+alexandreev92@users.noreply.github.com>
2021-12-02 00:53:22 +0400

    Add files via upload

---
+++ mesta/js/main.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu__item'),
+        hamburger = document.querySelector('.string__hamburger'),
+        btnmain = document.querySelectorAll('.btn-main');
+
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('menu--active');
+        hamburger.classList.toggle('active');
+    });
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.toggle('menu--active');
+            hamburger.classList.toggle('string__hamburger--active');
+        })
+    });
+
+    btnmain.forEach(item => {
+        item.addEventListener('click', () => {
+            btnmain.classList.toggleClass('active');
+        })
+    })
+
+
+    // btnmain.addEventListener('click', () => {
+    //     btnmain.classList.toggle('active');
+    // })
+
+
+})
+
+// $(function () {
+//     $('.string__hamburger').on('click', function () {
+//         $('.menu').addClass('.menu--active');
+//     });
+
+
+// }) 
