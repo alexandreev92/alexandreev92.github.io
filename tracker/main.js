2f2562c46a878d091a0ff0410b3939a7f886e044
alexandreev92 <93050970+alexandreev92@users.noreply.github.com>
2023-03-10 14:37:21 +0300

    Update main.js

--- tracker/main.js
+++ tracker/main.js
@@ -17,18 +17,40 @@ let voyagerMap = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voya
 
 
 //google map
-let googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=s,s&x={x}&y={y}&z={z}',{
+let googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
 });
 // googleStreets.addTo(map)
 
+let OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+let CyclOSM = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+	maxZoom: 20,
+	attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var EsriSatelite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
 // let other = L.layerGroup([lg_units, tr_places, cs_places]).addTo(map)
+
+var OSM_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+});
 
 let Basemaps = {
         "OSM": osm,
+        "OSM-HOT": OSM_HOT,
+        "Велокарта": CyclOSM,
         "Voyager": voyagerMap,
-        "Google": googleStreets
+        "Топокарта": OpenTopoMap,
+        "Google": googleStreets,
+        "ERSI(Спутник)": EsriSatelite,
     }
 L.control.layers(Basemaps).addTo(map);
 
